db471a5dfefe9284fe2ee2cbd4b7bbc22b5d3262
Alice_Mars <yaogongtonghua@foxmail.com>
2021-01-23 13:43:56 +0800

    unfinish

---
+++ FindStr.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+string Operation(string str,string op)
+{
+	int i,j,len;
+	len=op[2]-'0';
+	i=op[1]-'0';
+	j=op[1]-'0'+len-1;
+	if(op[0]=='0')			//·­×ª
+	{	
+		char c;
+		while(i<j)
+		{
+			c=str[i];
+			str[i]=str[j];
+			str[j]=c;
+			++i;
+			--j;
+		}
+		
+	}else if(op[0]=='1'){  //Ìæ»» 
+		int cc=3;
+		while(i<=j)
+		{
+			str[i]=op[cc];
+			++cc;
+			++i;
+		}
+	} 
+	return str;
+}
+int main()
+{
+	string str,op;
+	int n;
+	cin >> str;
+	cin >> n;
+	while(n--)
+	{
+		cin >> op;
+		str=Operation(str,op);
+		cout << str <<endl;
+	}
+}
+//181wtr
+0123456789
+jkygexrrwtrunuwxalgcbxistydvrxmfyhb
+jkygexrrw  unuwxalgcbxistydvrxmfyhb
+//009
+wrrxegykjtrunuwxalgcbxistydvrxmfyhb
+wrrxegykjunuwxalgcbxistydvrxmfyhb
